260fa48d076a778de5b954b373ef8a3939196262
Mutoi01 <pngando9@gmail.com>
2022-07-26 12:31:45 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route} from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <NavBar/>
+    <BrowserRouter>
+    <Route exact path="/actors">
+        <Actors/>
+      </Route>
+    <Route exact path="/directors">
+        <Directors/>
+      </Route>
+    <Route exact path="/movies">
+        <Movies/>
+      </Route>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      </BrowserRouter>
+  </div>;
 }
 
 export default App;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const allDirectors = directors.map((value) => (
+    <div key={value.name}>
+      <h2>{value.name}</h2>
+
+      <ul>
+        {value.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+     ));
+
+  return <div>{/*{code here}*/}
+  <h1>Directors Page</h1>
+    {allDirectors}
+  </div>;
 }
 
 export default Directors;
